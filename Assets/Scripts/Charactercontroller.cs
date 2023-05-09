a2932edfdb4c4dbd2b11da509bd4487b5bea7fd3
Coolguybrad <bnwicks03@gmail.com>
2023-05-09 14:38:02 +0200

    Movement
    Left and right movement added, jump to be fixed

--- Assets/Scripts/Charactercontroller.cs
+++ Assets/Scripts/Charactercontroller.cs
@@ -1,18 +1,56 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
-public class Charactercontroller : MonoBehaviour
+public class CharacterController : MonoBehaviour
 {
     // Start is called before the first frame update
-    void Start()
+    private CustomInput input = null;
+    private Vector3 move = Vector3.zero;
+    public Rigidbody rb;
+    private float speed = 10f;
+    public float jump = 1f;
+
+
+    private void Awake()
     {
-        
+        input = new CustomInput();
+
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnEnable()
     {
-        
+        input.Enable();
+        input.PlayerVert.Movement.performed += onMovement;
+        input.PlayerVert.Movement.canceled += onMovementStopped;
+    }
+
+    private void FixedUpdate()
+    {
+        rb.velocity = move * speed;
+        print(rb.velocity.y);
+        if (rb.velocity.y == 0)
+        {
+            rb.AddForce(Vector3.up * jump, ForceMode.Impulse);
+        }
+
+
+
+    }
+
+    private void OnDisable()
+    {
+        input.Disable();
+        input.PlayerVert.Movement.performed -= onMovement;
+        input.PlayerVert.Movement.canceled -= onMovementStopped;
+    }
+
+    private void onMovement(InputAction.CallbackContext v)
+    {
+        move = v.ReadValue<Vector3>();
+    }
+
+    private void onMovementStopped(InputAction.CallbackContext v)
+    {
+        move = Vector3.zero;
     }
 }
