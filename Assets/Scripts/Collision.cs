935b79996aa61ff998477f586773ada0e58bb8b0
Kerwin Naidoo <kerwinjn18@gmail.com>
2023-05-15 13:41:44 +0200

    Added ending triggerr

--- Assets/Scripts/Collision.cs
+++ Assets/Scripts/Collision.cs
@@ -4,18 +4,15 @@ using UnityEngine;
 
 public class Collision : MonoBehaviour
 {
+
+    public GameObject seedEnd;
+    public GameObject flowerEnd;
+    public GameObject treeEnd;
+
+    public GameObject cam;
+
     // Start is called before the first frame update
     public GameObject gameMan;
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 
     private void OnTriggerEnter(Collider other)
     {
@@ -25,6 +22,25 @@ public class Collision : MonoBehaviour
             gameMan.GetComponent<ScoreManager>().currentScore++;
             Destroy(other.gameObject); //destroys the pickup object
 
+        }
+
+        if(other.gameObject.tag == "Player")
+        {
+            if(gameMan.GetComponent<ScoreManager>().currentScore<5)
+            {
+                seedEnd.SetActive(true);                
+            }
+            else if(gameMan.GetComponent<ScoreManager>().currentScore < 10)
+            {
+                flowerEnd.SetActive(true);
+            }
+            else
+            {
+                treeEnd.SetActive(true) ;
+            }
+
+            other.gameObject.SetActive(false);
+            cam.SetActive(true);
         }
     }
 }
